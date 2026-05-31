89c5c1a5cf74de94dcb3a5c740a55fc6cfa934d4
Katerina Kashleva <40360807+ketrinFox5@users.noreply.github.com>
2026-05-31 17:22:03 +0400

    fix mock-data

--- lib/mock-data/journal-entries.ts
+++ lib/mock-data/journal-entries.ts
@@ -1,17 +1,17 @@
 import { JournalEntry } from "@/types/journal-entry"
 
 export const journalEntries: JournalEntry[] = [
-  {
-    id: "1",
-    createdAt: "2026-05-29",
-    content:
-      "Today I noticed how much energy I waste trying to control things outside myself.",
-  },
+  // {
+  //   id: "1",
+  //   createdAt: "2026-05-29",
+  //   answer:
+  //     "Today I noticed how much energy I waste trying to control things outside myself.",
+  // },
 
-  {
-    id: "2",
-    createdAt: "2026-05-28",
-    content:
-      "I reacted emotionally too quickly during a difficult conversation.",
-  },
+  // {
+  //   id: "2",
+  //   createdAt: "2026-05-28",
+  //   answer:
+  //     "I reacted emotionally too quickly during a difficult conversation.",
+  // },
 ]
